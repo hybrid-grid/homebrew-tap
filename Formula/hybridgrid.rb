92ec4fbd20f0c4b8d27849833ebcb638ed34e4f3
h3nr1.d14z <leduchieu101@gmail.com>
2026-01-19 16:45:30 +0700

    chore: bump to v0.1.1 with Windows support
    - Update Homebrew formula with new checksums
    - Update Scoop manifest for Windows users
    - Includes MSVC compiler support, Windows Service, WSL2 Docker

--- Formula/hybridgrid.rb
+++ Formula/hybridgrid.rb
@@ -5,28 +5,28 @@
 class Hybridgrid < Formula
   desc "Distributed multi-platform build system"
   homepage "https://github.com/hybrid-grid/hybridgrid"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/hybrid-grid/hybridgrid/releases/download/v0.1.0/hybridgrid_0.1.0_darwin_amd64.tar.gz"
-      sha256 "3d3c218b1878e479a87bfff7322661a49a5ce0b5d13d78cf2732eae007f9f735"
+      url "https://github.com/hybrid-grid/hybridgrid/releases/download/v0.1.1/hybridgrid_0.1.1_darwin_amd64.tar.gz"
+      sha256 "52a8ccb5b89eb3b25727d1327327b605fb553063ce191c4d02599193070332e7"
     end
     on_arm do
-      url "https://github.com/hybrid-grid/hybridgrid/releases/download/v0.1.0/hybridgrid_0.1.0_darwin_arm64.tar.gz"
-      sha256 "35e3ecb7edf5da1786cb0ac1e121bf3c4563aa99e8e22b369ca13deea9b44490"
+      url "https://github.com/hybrid-grid/hybridgrid/releases/download/v0.1.1/hybridgrid_0.1.1_darwin_arm64.tar.gz"
+      sha256 "4945ea50c9863f5a40d69760f3941f251a07457bb7d5a3bce9faccfb6e4e46b8"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/hybrid-grid/hybridgrid/releases/download/v0.1.0/hybridgrid_0.1.0_linux_amd64.tar.gz"
-      sha256 "f46663c94dfe0552326dadf1c189eaff39b5265a8e0591b60032140dfb437b42"
+      url "https://github.com/hybrid-grid/hybridgrid/releases/download/v0.1.1/hybridgrid_0.1.1_linux_amd64.tar.gz"
+      sha256 "9765dbc93a7caa9824c7e908508a2f8939586a330c4dd6a1f23294a9d92ae3a5"
     end
     on_arm do
-      url "https://github.com/hybrid-grid/hybridgrid/releases/download/v0.1.0/hybridgrid_0.1.0_linux_arm64.tar.gz"
-      sha256 "271baafbcc51caba9a1c776521bb3a0072e466a29609b5b7457c265c03b37319"
+      url "https://github.com/hybrid-grid/hybridgrid/releases/download/v0.1.1/hybridgrid_0.1.1_linux_arm64.tar.gz"
+      sha256 "2a71effda308b4f75b99d52392b527fc953cee06e9ee05d22b2906e068e0eec6"
     end
   end
 
